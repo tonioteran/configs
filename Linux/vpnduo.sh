d9f6634ec6615dedf36eeb8caa35dc98ef9798dd
antonio teran <teran@mit.edu>
2018-06-27 21:47:16 -0700

    add vpn shell

---
+++ Linux/vpnduo.sh
@@ -0,0 +1,10 @@
+##
+## Script for running Cisco Anyconnect to
+## join the MIT VPN with duo auth
+##
+## Antonio Teran
+## 06/27/18
+##
+
+# Run the cisco app:
+/opt/cisco/anyconnect/bin/vpn -s connect vpn.mit.edu/duo
