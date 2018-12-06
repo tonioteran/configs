742aadf83172b0f2b262843d1d43ed8a0d961b5f
antonio teran <teran@mit.edu>
2018-12-06 10:51:45 -0500

    add latex install

---
+++ Linux/installLatexTools.sh
@@ -0,0 +1,17 @@
+# ######################
+# 
+# installLatexTools.sh
+#
+# Contents:
+# - Latex compilers
+# ######################
+
+#!/usr/bin/env bash
+
+sudo apt update
+
+# install Latex compilers (long, ~5GB)
+sudo apt-get install texlive-full
+sudo apt-get install latexmk
+
+
