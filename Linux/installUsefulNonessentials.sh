5b0c2dc6418214c8a47b114613cf48efb352e2e3
antonio teran <teran@mit.edu>
2018-12-06 10:52:42 -0500

    init nonessential

---
+++ Linux/installUsefulNonessentials.sh
@@ -0,0 +1,23 @@
+# ######################
+# 
+# installUsefulNonessentials.sh
+#
+# Contents:
+# - Ubuntu Tweak Tool
+# - Terminator
+# - VLC
+# ######################
+
+
+#!/usr/bin/env bash
+
+# - Ubuntu Tweak Tool ($ gnome-tweaks)
+sudo add-apt-repository universe
+sudo apt install gnome-tweak-tool
+
+# install Terminator
+sudo apt-get install terminator
+
+# install VLC
+sudo apt-get install vlc
+
