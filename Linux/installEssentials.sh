1f7461ee938d072879154828c11c9d2a80ffbff4
antonio teran <teran@mit.edu>
2018-12-07 08:46:25 -0500

    update readme and essentials with eigen

--- Linux/installEssentials.sh
+++ Linux/installEssentials.sh
@@ -3,7 +3,6 @@
 # installEssentials.sh
 #
 # Contents:
-# - Terminator
 # ######################
 
 #!/usr/bin/env bash
@@ -19,15 +18,63 @@ sudo apt-get install htop
 # install curl
 sudo apt-get install libcurl4-openssl-dev
 
+# install dev tools
+sudo apt-get install unzip pkg-config
+
 # install CMake (version 3.13.1)
+echo "Installing CMake from source (3.13.1) ..."
 cd ~/Downloads
 wget https://github.com/Kitware/CMake/releases/download/v3.13.1/cmake-3.13.1.tar.gz
 tar zxvf cmake-3.13.1.tar.gz
-cd cmake-3.13.1.tar.gz
-./bootstrap && make -j4 && sudo make install
+cd cmake-3.13.1
+./bootstrap --system-curl && make -j4 && sudo make install
 cd ~/Downloads
 rm -rf cmake-3.13.1*
 
+# install pythons
+echo "Installing python 2 and 3..."
+sudo apt-get install python3.7-dev
+sudo apt-get install python2.7-dev
+sudo apt install python3-widgetsnbextension
+sudo apt install python3-testresources
+# install pip
+cd ~/Downloads
+wget https://bootstrap.pypa.io/get-pip.py
+sudo python3 get-pip.py
+rm get-pip.py
+sudo pip install numpy
+
+# install Boost library
+echo "Installing Boost library from source (1.68.0) ..."
+cd ~/Downloads
+wget https://dl.bintray.com/boostorg/release/1.68.0/source/boost_1_68_0.tar.gz
+tar zxvf boost_1_68_0.tar.gz
+cd boost_1_68_0
+sudo ./bootstrap.sh
+sudo ./b2 install
+cd ~/Downloads
+sudo rm -rf boost_1_68*
+
+# install OpenGL
+echo "Installing OpenGL libraries"
+sudo apt-get install libgl1-mesa-dev
+sudo apt-get install libglew-dev libsdl2-dev libsdl2-image-dev libglm-dev libfreetype6-dev
+sudo apt install mesa-utils
+sudo apt-get install freeglut3 freeglut3-dev
+
+
+# install Eigen
+cd /usr/local/include
+sudo wget http://bitbucket.org/eigen/eigen/get/3.3.5.tar.gz
+sudo tar zxvf 3.3.5.tar.gz
+cd eigen-eigen-b3f3d4950030/
+sudo mkdir build
+cd build
+sudo cmake ..
+sudo make install
+cd /usr/local/include
+sudo rm 3.3.5.tar.gz
+sudo rm -rf eigen-eigen-b3f3d4950030/
 
 
 
--- Linux/readme.md
+++ Linux/readme.md
@@ -6,11 +6,30 @@ I've chopped up the distinct installation steps into small bites so that I can p
 
 - `installEssentials.sh`
 
+-----
+
 - `installUsefulNonessentials.sh`
 
 This script will install nifty nonessential packages, such as:
+
   - Gnome Tweak Tool. Allows to more granularly configure the OS.
 
+-----
+
+- `masterInstall.sh`
+
+Combines all of the previous scripts into a master installer.
+
 ## Manual installations
+
+Scripts that are not included within the master installer are the following:
+
+### Corsair Keyboard Software
+
+Clones the Github repo, installs dependencies, and installs the driver for the Corsair keyboard. File: `installCorsair.sh`.
+
+
+
+
 
 
