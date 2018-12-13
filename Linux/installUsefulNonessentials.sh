adafe9d51908ac0f1c45d28268dff668452eb896
antonio teran <teran@mit.edu>
2018-12-13 09:59:46 -0500

    add pangolin to nonessentials

--- Linux/installUsefulNonessentials.sh
+++ Linux/installUsefulNonessentials.sh
@@ -21,3 +21,21 @@ sudo apt-get install terminator
 # install VLC
 sudo apt-get install vlc
 
+# get Pangolin for OpenGL management:
+cd ~/repos
+git clone https://github.com/stevenlovegrove/Pangolin.git
+cd Pangolin
+# -- get dependencies
+sudo apt-get install libglew-dev
+sudo apt-get install ffmpeg libavcodec-dev libavutil-dev libavformat-dev libswscale-dev libavdevice-dev
+sudo apt-get install libjpeg-dev libtiff5-dev libopenexr-dev
+# -- build and install
+mkdir build
+cd build
+cmake ..
+cmake --build .
+sudo make install
+
+
+
+
