00fb1c8ae4b68cfda0e89c6049ab6504e1d22771
antonio teran <teran@mit.edu>
2019-05-30 19:46:04 -0700

    add qt creator

---
+++ Linux/installQtCreator.sh
@@ -0,0 +1,20 @@
+# ##########################
+# 
+# installQtCreator.sh
+#
+# grab needed packages
+# ##########################
+
+#!/usr/bin/env bash
+
+# requirements
+sudo apt install build-essential
+sudo apt install qtcreator
+sudo apt install qt5-default
+
+# install documentation and examplesj
+sudo apt install qt5-doc
+sudo apt install qt5-doc-html qtbase5-doc-html
+sudo apt install qtbase5-examples
+
+
