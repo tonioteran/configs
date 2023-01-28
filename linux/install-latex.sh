0fedad6b2dc22679c061e5d2220c373fba5ccd84
= <toniioteran@gmail.com>
2023-01-28 13:07:49 -0800

    Rename latex installer script.

---
+++ linux/install-latex.sh
@@ -0,0 +1,12 @@
+# ######################
+# Contents:
+# - Latex compilers
+# ######################
+
+#!/usr/bin/env bash
+
+sudo apt update
+
+# Install Latex compilers (long, ~5GB).
+sudo apt install texlive-full
+sudo apt install latexmk
